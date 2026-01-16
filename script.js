b37a3285a0c31a5c589c10958de045f6ca4defa4
elbs8845 <elbs8845@gmail.com>
2026-01-16 10:20:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function login(){
-  let u = document.getElementById("login").value;
-  let s = document.getElementById("senha").value;
+// USUÁRIOS PADRÃO
+let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+if(!usuarios){
+  usuarios = [
+    { login: "admin", senha: "123", tipo: "admin" }
+  ];
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+}
+
+// LOGIN
+document.getElementById("btnLogin").addEventListener("click", function(){
+
+  let u = document.getElementById("login").value.trim();
+  let s = document.getElementById("senha").value.trim();
 
   let user = usuarios.find(x => x.login === u && x.senha === s);
+
   if(!user){
     alert("Usuário ou senha inválidos");
     return;
@@ -15,4 +28,10 @@ function login(){
   } else {
     window.location.href = "vendedor.html";
   }
+});
+
+// LOGOUT
+function sair(){
+  localStorage.removeItem("logado");
+  window.location.href = "index.html";
 }
