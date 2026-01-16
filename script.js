a7d491ef725cea4600a4b8258212ab280ed8d3c2
elbs8845 <elbs8845@gmail.com>
2026-01-16 14:31:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -162,3 +162,26 @@ function delVenda(i){
   localStorage.setItem("vendas", JSON.stringify(vendas));
   listarVendas();
 }
+
+function salvarMeta(){
+  const meta = document.getElementById("meta");
+
+  if(!meta || !meta.value){
+    alert("Informe a meta");
+    return;
+  }
+
+  localStorage.setItem("meta", meta.value);
+  alert("Meta salva");
+}
+function salvarMeta(){
+  const meta = document.getElementById("meta");
+
+  if(!meta || !meta.value){
+    alert("Informe a meta");
+    return;
+  }
+
+  localStorage.setItem("meta", meta.value);
+  alert("Meta salva");
+}
